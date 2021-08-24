f420d1b6744864146939fd4826b5b8302fbf86aa
acarreno <jeison-c13@hotmail.com>
2021-08-24 07:21:05 -0500

    cambios test

--- CeibaAdn-ParkingUITests/CeibaAdn_ParkingUITests.swift
+++ CeibaAdn-ParkingUITests/CeibaAdn_ParkingUITests.swift
@@ -9,7 +9,7 @@ import XCTest
 
 class CeibaAdn_ParkingUITests: XCTestCase {
 
-    override func setUpWithError() throws {
+   /* override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
@@ -39,4 +39,6 @@ class CeibaAdn_ParkingUITests: XCTestCase {
             }
         }
     }
+    */
+    
 }

633982acb8deed138e202d9f9c10c0d076d9abde
andreyCarreno <andrey.carreno@ceiba.com.co>
2021-08-23 20:10:05 -0500

    modification test

--- CeibaAdn-ParkingUITests/CeibaAdn_ParkingUITestsLaunchTests.swift
+++ CeibaAdn-ParkingUITests/CeibaAdn_ParkingUITestsLaunchTests.swift
@@ -38,9 +38,9 @@ class CeibaAdn_ParkingUITestsLaunchTests: XCTestCase {
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
             // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
+            /*measure(metrics: [XCTApplicationLaunchMetric()]) {
                 XCUIApplication().launch()
-            }
+            }*/
         }
     }
     

0725983351774b9474e81c8d2ea4f6ebc71b0ed2
andreyCarreno <andrey.carreno@ceiba.com.co>
2021-08-23 19:50:02 -0500

    modification jenkins file

--- CeibaAdn-ParkingUITests/CeibaAdn_ParkingUITestsLaunchTests.swift
+++ CeibaAdn-ParkingUITests/CeibaAdn_ParkingUITestsLaunchTests.swift
@@ -9,12 +9,41 @@ import XCTest
 
 class CeibaAdn_ParkingUITestsLaunchTests: XCTestCase {
 
-    override class var runsForEachTargetApplicationUIConfiguration: Bool {
-        true
-    }
+//    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+//        true
+//    }
 
     override func setUpWithError() throws {
         continueAfterFailure = false
     }
 
+    override func tearDownWithError() throws {
+        
+    }
+    
+    func testExample() throws{
+        let app = XCUIApplication()
+        app.launch()
+    }
+    
+    func testLaunchPerformance() throws {
+        
+            measure(metrics: [XCTApplicationLaunchMetric()]) {
+                XCUIApplication().launch()
+            }
+        
+    }
+    
+//    func testLaunch() throws {
+//        let app = XCUIApplication()
+//        app.launch()
+//
+//        // Insert steps here to perform after app launch but before taking a screenshot,
+//        // such as logging into a test account or navigating somewhere in the app
+//
+//        let attachment = XCTAttachment(screenshot: app.screenshot())
+//        attachment.name = "Launch Screen"
+//        attachment.lifetime = .keepAlways
+//        add(attachment)
+//    }
 }
